a0d19e5356e1a2023d3d99bfa4369085a429280d
Joe Muthu <joemmuthu@gmail.com>
2016-12-01 19:40:43 +0530

    Fixed navigation ( to right page) when home is selected in plant detail page

--- app/src/main/java/com/github/jmuthu/poovali/activity/PlantDetailActivity.java
+++ app/src/main/java/com/github/jmuthu/poovali/activity/PlantDetailActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -177,7 +176,7 @@ public class PlantDetailActivity extends AppCompatActivity {
                 startActivity(intent);
                 return true;
             case android.R.id.home:
-                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
